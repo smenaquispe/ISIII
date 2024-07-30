eb1d0a955a86ecefab6451aca9c946fc7193b9ce
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2024-07-30 14:24:48 -0500

    Implementar repositorio para la persistencia de presupuestos

--- gestion_presupuestal/repositories.py
+++ gestion_presupuestal/repositories.py
@@ -1,14 +1,18 @@
 from .models import Presupuesto
 
 class PresupuestoRepository:
+    def __init__(self):
+        self.presupuestos = {}
+
     def adicionar(self, presupuesto: Presupuesto) -> None:
-        pass
+        self.presupuestos[presupuesto.id] = presupuesto
 
     def eliminar(self, id) -> None:
-        pass
+        if id in self.presupuestos:
+            del self.presupuestos[id]
 
     def actualizar(self, presupuesto: Presupuesto) -> None:
-        pass
+        self.presupuestos[presupuesto.id] = presupuesto
 
     def buscar(self, id) -> Presupuesto:
-        pass
+        return self.presupuestos.get(id)
