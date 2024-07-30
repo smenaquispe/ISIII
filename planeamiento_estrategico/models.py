4706cef0771097933a4fb1a4384900d0ca6ee8c8
smena <sergiosantosmenaq@gmail.com>
2024-07-30 14:39:00 -0500

    agregando modelo estrategia

--- planeamiento_estrategico/models.py
+++ planeamiento_estrategico/models.py
@@ -1,15 +1,14 @@
 from uuid import uuid4
 
-class Presupuesto:
-    def __init__(self, nombre: str, descripcion: str, importe: float, aprobado: bool) -> None:
+class Estrategia:
+    def __init__(self, nombre: str, descripcion: str, aprobado: bool) -> None:
         self.id: str = str(uuid4())
         self.nombre: str = nombre
         self.descripcion: str = descripcion
-        self.importe: float = importe
         self.aprobado: bool = aprobado
 
-    def crear_presupuesto(self) -> None:
+    def crear_estrategia(self) -> None:
         pass
 
-    def aprobar_presupuesto(self) -> None:
+    def evaluar_estrategia(self) -> None:
         pass
