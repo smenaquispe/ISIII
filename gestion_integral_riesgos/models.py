b2ad11818f5d953b6e4dbbfb1b206e7e24fd0edc
ChristianPE1 <cpardave@unsa.edu.pe>
2024-08-01 16:28:28 -0500

    Add: prioridad_alta al modelo

--- gestion_integral_riesgos/models.py
+++ gestion_integral_riesgos/models.py
@@ -1,20 +1,21 @@
-from uuid import uuid4
 from flask_sqlalchemy import SQLAlchemy
 
 db_riesgos = SQLAlchemy()
 
 class Riesgo(db_riesgos.Model):
 
-    id = db_riesgos.Column(db_riesgos.String, primary_key=True)
+    id = db_riesgos.Column(db_riesgos.Integer, primary_key=True)
     nombre = db_riesgos.Column(db_riesgos.String(80), nullable=False)
     descripcion = db_riesgos.Column(db_riesgos.String(200), nullable=False)
     probabilidad = db_riesgos.Column(db_riesgos.Float, nullable=False)
+    prioridad_alta = db_riesgos.Column(db_riesgos.Boolean, nullable=False,default=False)
 
-    def __init__(self, nombre: str, descripcion: str, probabilidad: float):
-        self.id: str = str(uuid4())
+    def __init__(self,id: int, nombre: str, descripcion: str, probabilidad: float):
+        self.id: int = id
         self.nombre: str = nombre
         self.descripcion: str = descripcion
         self.probabilidad: float = probabilidad
+        self.prioridad_alta: bool = False
 
     def crear_riesgo(self) -> None:
         pass
