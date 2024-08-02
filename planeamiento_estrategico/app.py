dff520d0c8aaa95bb51896bf1793fd6009fd4798
smena <sergiosantosmenaq@gmail.com>
2024-08-01 22:18:06 -0500

    agregando swagger

--- planeamiento_estrategico/app.py
+++ planeamiento_estrategico/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Estrategias API',
+          description='A simple API for managing estrategias', doc='/swagger')
+
+ns = api.namespace('estrategias', description='Operations related to estrategias')
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
--- planeamiento_estrategico/controllers.py
+++ planeamiento_estrategico/controllers.py
@@ -1,9 +1,61 @@
 from flask import request, jsonify
-from app import app, db
+from flask_restx import Api, Resource, fields
+from app import app, db, api, ns
 from models import Estrategia
 from repositories import EstrategiaRepository
 
 repo = EstrategiaRepository()
+
+estrategia_model = api.model('Estrategia', {
+    'nombre': fields.String(required=True, description='The name of the estrategia'),
+    'descripcion': fields.String(required=True, description='The description of the estrategia')
+})
+
+@ns.route('/')
+class EstrategiaList(Resource):
+    @ns.doc('create_estrategia')
+    @ns.expect(estrategia_model)
+    @ns.response(201, 'Estrategia creada')
+    def post(self):
+        '''Create a new estrategia'''
+        data = request.get_json()
+        estrategia = Estrategia(nombre=data['nombre'], descripcion=data['descripcion'])
+        repo.adicionar(estrategia)
+        return {"message": "Estrategia creada"}, 201
+
+@ns.route('/<int:id>')
+@ns.response(404, 'Estrategia no encontrada')
+class Estrategia(Resource):
+    @ns.doc('get_estrategia')
+    @ns.marshal_with(estrategia_model)
+    def get(self, id):
+        '''Fetch a estrategia given its identifier'''
+        estrategia = repo.buscar(id)
+        if estrategia:
+            return estrategia, 200
+        api.abort(404, "Estrategia no encontrada")
+
+    @ns.doc('delete_estrategia')
+    @ns.response(204, 'Estrategia eliminada')
+    def delete(self, id):
+        '''Delete a estrategia given its identifier'''
+        repo.eliminar(id)
+        return {"message": "Estrategia eliminada"}, 204
+
+    @ns.doc('update_estrategia')
+    @ns.expect(estrategia_model)
+    @ns.response(200, 'Estrategia actualizada')
+    def put(self, id):
+        '''Update a estrategia given its identifier'''
+        data = request.get_json()
+        estrategia = repo.buscar(id)
+        if estrategia:
+            estrategia.nombre = data['nombre']
+            estrategia.descripcion = data['descripcion']
+            repo.actualizar(estrategia)
+            return {"message": "Estrategia actualizada"}, 200
+        api.abort(404, "Estrategia no encontrada")
+
 
 @app.route('/estrategias', methods=['POST'])
 def create_estrategia():
@@ -34,3 +86,5 @@ def update_estrategia(id):
         repo.actualizar(estrategia)
         return jsonify({"message": "Estrategia actualizada"}), 200
     return jsonify({"message": "Estrategia no encontrada"}), 404
+
+api.add_namespace(ns)
