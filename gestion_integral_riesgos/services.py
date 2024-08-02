c21c7bcbf3e6db99ce7e5ec91f72f8269b3582d4
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-08-02 08:32:11 -0500

    Delete gestion_integral_riesgos directory

--- gestion_integral_riesgos/services.py
+++
@@ -1,9 +0,0 @@
-class MonitoreoService:
-    def __init__(self):
-        self.informe_riesgo: str = ""
-
-    def generar_informe(self) -> None:
-        pass
-
-    def identificar_mejora(self) -> None:
-        pass
