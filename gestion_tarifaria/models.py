62190ca2f3532ea204042a29b610625d9c9b8fd6
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-08-02 08:59:53 -0500

    Update models.py

--- gestion_tarifaria/models.py
+++ gestion_tarifaria/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from app import db
 
 class Tarifario:
     def __init__(self, nombre: str, descripcion: str, importe: float, aprobado: bool) -> None:
@@ -8,8 +9,12 @@ class Tarifario:
         self.importe: float = importe
         self.aprobado: bool = aprobado
 
-    def crear_tarifario(self) -> None:
-        pass
+class Tarifa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(200), nullable=False)
+    importe = db.Column(db.Float, nullable=False)  # Si quieres añadir un campo para el importe
+
 
     def aprobar_tarifario(self) -> None:
         pass
