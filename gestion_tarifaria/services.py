9a5c33af4a7c1b1f6295cf6761065c87a248f3db
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-08-02 08:32:35 -0500

    Delete gestion_tarifaria directory

--- gestion_tarifaria/services.py
+++
@@ -1,9 +0,0 @@
-class TarifarialService:
-    def __init__(self):
-        self.informe_tarifario: str = ""
-    
-    def verificar_tarifario(self) -> None:
-        pass
-
-    def resolucion_informe(self) -> None:
-        pass
