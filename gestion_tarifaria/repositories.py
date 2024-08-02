1cecc3a588e3c916ee9d7786165c14c717f2738b
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-08-02 09:00:41 -0500

    Update repositories.py

--- gestion_tarifaria/repositories.py
+++ gestion_tarifaria/repositories.py
@@ -1,14 +1,20 @@
-from .models import Tarifario
+from app import db
+from models import Tarifa
 
-class TarifarioRepository:
-    def adicionar(self, tarifario: Tarifario) -> None:
-        pass
+class TarifaRepository:
+    def adicionar(self, tarifa: Tarifa) -> None:
+        db.session.add(tarifa)
+        db.session.commit()
 
-    def eliminar(self, id) -> None:
-        pass
+    def eliminar(self, id: int) -> None:
+        tarifa = Tarifa.query.get(id)
+        if tarifa:
+            db.session.delete(tarifa)
+            db.session.commit()
 
-    def actualizar(self, tarifario: Tarifario) -> None:
-        pass
+    def actualizar(self, tarifa: Tarifa) -> None:
+        db.session.merge(tarifa)
+        db.session.commit()
 
-    def buscar(self, id) -> Tarifario:
-        pass
+    def buscar(self, id: int) -> Tarifa:
+        return Tarifa.query.get(id)
