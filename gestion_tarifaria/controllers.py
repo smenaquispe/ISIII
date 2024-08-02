29fee40caa70a1e9061ba1d78eb26de75ed86207
Paolo-Daniel-B-A <104223268+Paolo-Daniel-B-A@users.noreply.github.com>
2024-08-02 08:57:28 -0500

    Update controllers.py

--- gestion_tarifaria/controllers.py
+++ gestion_tarifaria/controllers.py
@@ -1,30 +1,91 @@
-from flask import Blueprint, request, jsonify
-from .factory import TarifarioFactory
-from .repositories import TarifarioRepository
-from .services import TarifarialService
+from flask import request, jsonify
+from flask_restx import Api, Resource, fields
+from app import app, db, api, ns
+from models import Tarifa
+from repositories import TarifaRepository
 
-tarifarios_bp = Blueprint('tarifarios', __name__)
-tarifario_repo = TarifarioRepository()
-tarifarial_service = TarifarialService()
+repo = TarifaRepository()
 
-@tarifarios_bp.route('/crear', methods=['POST'])
-def crear_tarifario():
-    data = request.json
-    nuevo_tarifario = TarifarioFactory.crear_tarifario(
-        nombre=data['nombre'],
-        descripcion=data['descripcion'],
-        importe=data['importe'],
-        aprobado=data['aprobado']
-    )
-    tarifario_repo.adicionar(nuevo_tarifario)
-    return jsonify({'message': 'Tarifa creado'}), 201
+tarifa_model = api.model('Tarifa', {
+    'nombre': fields.String(required=True, description='The name of the tarifa'),
+    'descripcion': fields.String(required=True, description='The description of the tarifa')
+})
+
+@ns.route('/')
+class TarifaList(Resource):
+    @ns.doc('create_tarifa')
+    @ns.expect(tarifa_model)
+    @ns.response(201, 'Tarifa creada')
+    def post(self):
+        '''Create a new tarifa'''
+        data = request.get_json()
+        tarifa = Tarifa(nombre=data['nombre'], descripcion=data['descripcion'])
+        repo.adicionar(tarifa)
+        return {"message": "Tarifa creada"}, 201
+
+@ns.route('/<int:id>')
+@ns.response(404, 'Tarifa no encontrada')
+class Tarifa(Resource):
+    @ns.doc('get_tarifa')
+    @ns.marshal_with(tarifa_model)
+    def get(self, id):
+        '''Fetch a tarifa given its identifier'''
+        tarifa = repo.buscar(id)
+        if tarifa:
+            return tarifa, 200
+        api.abort(404, "Tarifa no encontrada")
+
+    @ns.doc('delete_tarifa')
+    @ns.response(204, 'Tarifa eliminada')
+    def delete(self, id):
+        '''Delete a tarifa given its identifier'''
+        repo.eliminar(id)
+        return {"message": "Tarifa eliminada"}, 204
+
+    @ns.doc('update_tarifa')
+    @ns.expect(tarifa_model)
+    @ns.response(200, 'Tarifa actualizada')
+    def put(self, id):
+        '''Update a tarifa given its identifier'''
+        data = request.get_json()
+        tarifa = repo.buscar(id)
+        if tarifa:
+            tarifa.nombre = data['nombre']
+            tarifa.descripcion = data['descripcion']
+            repo.actualizar(tarifa)
+            return {"message": "Tarifa actualizada"}, 200
+        api.abort(404, "Tarifa no encontrada")
 
 
-@tarifarios_bp.route('/informe', methods=['GET'])
-def resolucion_informe():
-    informe = tarifarial_service.resolucion_informe()
-    return jsonify({'informe': informe})
+@app.route('/tarifas', methods=['POST'])
+def create_tarifa():
+    data = request.get_json()
+    tarifa = Tarifa(nombre=data['nombre'], descripcion=data['descripcion'])
+    repo.adicionar(tarifa)
+    return jsonify({"message": "Tarifa creada"}), 201
 
-# Agrega las rutas al módulo de Flask
-def init_app(app):
-    app.register_blueprint(tarifarios_bp, url_prefix='/tarifarios')
+@app.route('/tarifas/<int:id>', methods=['GET'])
+def read_tarifa(id):
+    tarifa = repo.buscar(id)
+    if tarifa:
+        return jsonify({"id": tarifa.id, "nombre": tarifa.nombre, "descripcion": tarifa.descripcion}), 200
+    return jsonify({"message": "Tarifa no encontrada"}), 404
+
+@app.route('/tarifas/<int:id>', methods=['DELETE'])
+def delete_tarifa(id):
+    repo.eliminar(id)
+    return jsonify({"message": "Tarifa eliminada"}), 204
+
+@app.route('/tarifas/<int:id>', methods=['PUT'])
+def update_tarifa(id):
+    data = request.get_json()
+    tarifa = repo.buscar(id)
+    if tarifa:
+        tarifa.nombre = data['nombre']
+        tarifa.descripcion = data['descripcion']
+        repo.actualizar(tarifa)
+        return jsonify({"message": "Tarifa actualizada"}), 200
+    return jsonify({"message": "Tarifa no encontrada"}), 404
+
+api.add_namespace(ns)
+
